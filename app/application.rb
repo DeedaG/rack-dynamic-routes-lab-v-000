7cf3a2e3cc6f501e38b0d878a81b137e165c75e0
Christia T <christiatidwell@bellsouth.net>
2018-08-26 02:04:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,44 +1,14 @@
 class Application
 
-  @@items = []
-
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
-        resp.write "#{price}\n"
-
-    elsif
-      resp.write "404"
-    end
-
-    resp.finish
-  end
-end
-
-
-class Application
-
-  @@items = []
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-
-    if req.path.match(/items/<ITEM NAME>)
-
-      search_term = req.params["Item Name"]
-
-      if Item.include?(search_term)
-        resp.write "#{Item.search_term.price}"
-      else
-        resp.write "404"
-      end
-
+    if req.path=="/songs"
+      resp.write "Item.price"
     else
-      resp.write "Path Not Found"
+      resp.write "Route not found"
+      resp.status = 404
     end
 
     resp.finish
