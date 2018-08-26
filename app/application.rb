f68c25400457f3b3d7609b47b8cad9f09f7918fb
Christia T <christiatidwell@bellsouth.net>
2018-08-26 01:47:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
         resp.write "#{item}\n"
       end
 
-    else req.path.!match(/items/<ITEM NAME>)
+    elsif req.path.!match(/items/<ITEM NAME>)
       resp.write "404"
     end
 
