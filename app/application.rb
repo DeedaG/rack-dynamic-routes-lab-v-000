fb6e4904ad17a1d2d1fb86cfca3dea161c81621b
Christia T <christiatidwell@bellsouth.net>
2018-08-26 02:59:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
         item = @@items.find{|i| i.name == item_name}
 
 
-        if item_name != item
+        if item != item_name
           resp.write "Item not found"
           resp.status = 400
 
