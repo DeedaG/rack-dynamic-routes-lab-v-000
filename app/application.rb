fa522a872887b858cd79e3285cf9c2a6cb2daf73
Christia T <christiatidwell@bellsouth.net>
2018-08-26 01:53:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/name:)
+    if req.path.match(/items/)
         resp.write "#{price}\n"
 
     elsif
